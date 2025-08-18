6da43f8abab39f89dd10a23d134823eb67988dff
fdjrn <fdjrn.dev@gmail.com>
2025-08-18 14:51:15 +0700

    fix: remove debug route

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/debug', function (Request $request) {
-    return response()->json($request->headers->all());
 });
