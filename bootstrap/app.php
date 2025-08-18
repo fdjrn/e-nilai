f6c9fb51e1f27fad33aafa8ee42742aea69a5552
fdjrn <fdjrn.dev@gmail.com>
2025-08-18 10:48:27 +0700

    fix: add condition on production env #3 (bootstrap/app/php)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        if (config('app.env') === 'production') {
+        if (($_ENV['APP_ENV'] ?? 'local') === 'production') {
             $middleware->append(TrustProxies::class);
         }
     })
