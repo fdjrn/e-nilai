1d31713f4ca4ef70e0e685e39aead9e97a57970b
fdjrn <fdjrn.dev@gmail.com>
2025-08-18 10:36:15 +0700

    fix: add condition on production env #2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,11 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
         if (config('app.env') === 'production') {
-             TrustProxies::class;
+            $middleware->append(TrustProxies::class);
         }
-
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
