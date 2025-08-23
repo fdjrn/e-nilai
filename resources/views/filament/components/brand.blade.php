70c4e721ec20b97ef244d2b442b3fcad3e439513
fdjrn <fdjrn.dev@gmail.com>
2025-08-23 10:44:55 +0700

    fix: publish filament vendor views

---
+++ resources/views/filament/components/brand.blade.php
@@ -0,0 +1,6 @@
+<div class="flex items-center gap-2">
+    <img src="{{ asset('images/logo-400x400.png') }}" alt="Logo" class="h-8">
+    <span class="text-lg font-medium text-gray-800 dark:text-gray-100">
+        SMAN 2 CIKARANG UTARA
+    </span>
+</div>
